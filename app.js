d4fc88f13bda1fd68fed2d0e5c94718c809a32d7
Sergey Puzankov <sergei.puzankov@gettyimages.com>
2016-08-04 21:09:42 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require( 'express' );
+var path = require( 'path' );
+var fs = require( 'fs' );
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+var apiVersion = '1.0';
+
+function render(req, res) {
+
+    var fileName = req.path + '/' + req.method.toLowerCase() + '.json';
+
+    fileName = fileName.replace('/' + apiVersion + '/', '/');
+
+    var filePath = path.join(__dirname, fileName);
+
+    console.log(req.method, filePath);
+
+    if (fs.existsSync(filePath)) {
+        res.setHeader('content-type', 'application/json');
+
+        fs.createReadStream(filePath).pipe(res);
+    }
+    else {
+        console.log('no such file', filePath);
+
+        res
+            .status(404)
+            .json([
+                {
+                    "info": {
+                        "success": false,
+                        "code": 12345
+                    }
+                }
+            ])
+            .end();
+    }
+}
+
+app.get('/', function (req, res) {
+    res.send('<html><body style="background-color: #ff0;"><h1>My web app http API 2.0!</h1></body></html>');
+});
+
+app.get('/test/', function (req, res) {
+    res.send('<html><body style="background-color: #ff0;"><h1>Hello ADV-FE2 !!!!!!</h1></body></html>');
+});
+
+app.all('/api/' + apiVersion + '/*', function (req, res) {
+    render(req, res);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
